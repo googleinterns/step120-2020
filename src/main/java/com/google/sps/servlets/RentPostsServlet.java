4b41719ce2206adb6f90551f337e1088845053a6
Shaina Chen <shaina.sierra@gmail.com>
2020-07-07 23:44:40 +0000

    Implement posting of rent document to Firestore

--- src/main/java/com/google/sps/servlets/RentPostsServlet.java
+++ src/main/java/com/google/sps/servlets/RentPostsServlet.java
@@ -14,12 +14,25 @@
 
 package com.google.sps;
 
+import static com.google.sps.Constants.INDEX_URL;
 import static com.google.sps.Constants.PROJECT_ID;
+import static com.google.sps.Constants.RENT_COLLECTION_NAME;
+import static com.google.sps.Constants.REQUEST_DESCRIPTION;
+import static com.google.sps.Constants.REQUEST_END_DATE;
+import static com.google.sps.Constants.REQUEST_LEASE_TYPE;
+import static com.google.sps.Constants.REQUEST_NUM_ROOMS;
+import static com.google.sps.Constants.REQUEST_PRICE;
+import static com.google.sps.Constants.REQUEST_ROOM_TYPE;
+import static com.google.sps.Constants.REQUEST_START_DATE;
+import static com.google.sps.Constants.REQUEST_TIMESTAMP;
+import static com.google.sps.Constants.REQUEST_TITLE;
 
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.FieldValue;
 import com.google.cloud.firestore.Firestore;
+import com.google.common.collect.ImmutableList;
 import com.google.firebase.cloud.FirestoreClient;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -32,10 +45,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
-/** Servlet that returns rent posts. */
+/** Servlet that posts and fetches rent posts. */
 @WebServlet("/rent-posts")
 public class RentPostsServlet extends HttpServlet {
   private Firestore db;
+  private ImmutableList<String> requestParams =
+    ImmutableList.of(REQUEST_DESCRIPTION, REQUEST_END_DATE, REQUEST_LEASE_TYPE,
+        REQUEST_NUM_ROOMS, REQUEST_PRICE, REQUEST_ROOM_TYPE, REQUEST_START_DATE,
+        REQUEST_TITLE);
 
   @Override
   public void init() throws ServletException {
@@ -49,17 +66,11 @@ public class RentPostsServlet extends HttpServlet {
   }
 
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
-  }
-
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Map<String, Object> data = new HashMap<>();
-    data.put("name", "San Francisco");
-    data.put("country", "United States");
-    ApiFuture<DocumentReference> addedDocRef = db.collection("cities").add(data);
+  public void doPost(HttpServletRequest request, HttpServletResponse response) 
+                      throws IOException {
+    Map<String, Object> rentDocument = createDocument(request, requestParams);
+    db.collection(RENT_COLLECTION_NAME).add(rentDocument);
+    response.sendRedirect(INDEX_URL);
   }
 
   private Firestore getFirestoreDatabase(GoogleCredentials credentials, String projectID) {
@@ -69,5 +80,15 @@ public class RentPostsServlet extends HttpServlet {
       .build();
     FirebaseApp.initializeApp(options);
     return FirestoreClient.getFirestore();
+  }
+
+  private Map<String, Object> createDocument(HttpServletRequest request, 
+                                              ImmutableList<String> requestParams) {
+    Map<String, Object> data = new HashMap<>();
+    for (String requestParam : requestParams) {
+      data.put(requestParam, request.getParameter(requestParam));
+    }
+    data.put(REQUEST_TIMESTAMP, FieldValue.serverTimestamp());
+    return data;
   }
 }
