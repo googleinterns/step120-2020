f6e943da9dea6953e17fe7340172ddd4ee0a34d1
Jortiz23 <jortiz23@verizon.net>
2020-07-18 01:09:12 +0000

    Refactor App to exclusively build the whole app rather than components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import React, { Component } from 'react';
-import Comment from './Comment/components/Comment';
-import CommentEditor from './CommentEditor/components/CommentEditor';
+import PostCommentsDisplay from './PostCommentsDisplay/components/PostCommentsDisplay';
+
 import './App.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.addComment = this.addComment.bind(this);
-
-        this.state = {
-            comments: [],
-        }
-    }
-
-    addComment(newCommentBody){
-        const newState = Object.assign({}, this.state);
-        newState.comments.push(newCommentBody);
-        this.setState(newState);
-    }
+    
 
     render(){
         return (
-            <div>
-                { 
-                    this.state.comments.map((commentBody, i) => {
-                        return (
-                            <Comment key={i} commentBody={commentBody}/>
-                        )
-                    })
-                }
-
-                <CommentEditor addComment={this.addComment} />
-            </div>
+            <PostCommentsDisplay />
         );
     }
 }
