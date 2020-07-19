45864a849f76cc761d9702d0e50d8010efc852fc
Shaina Chen <shaina.sierra@gmail.com>
2020-07-19 23:23:36 +0000

    Add support for automatic price calculation

--- src/main/webapp/resources/js/makeListing.js
+++ src/main/webapp/resources/js/makeListing.js
@@ -1,16 +1,11 @@
 function calculateListingPrice() {
-  console.log("testing");
-  let listingForm = document.forms["listingForm"].innerHTML;
   let numSingles = parseInt(document.getElementById("numSingles").value);
   let singlePrice = parseFloat(document.getElementById("singlePrice").value);
   let numShared = parseInt(document.getElementById("numShared").value);
   let sharedPrice = parseFloat(document.getElementById("sharedPrice").value);
   let listingPrice = numSingles * singlePrice + numShared * sharedPrice;
-  console.log(listingPrice);
-  console.log(isNaN(listingPrice));
-  console.log(listingPrice == "NaN");
   if (isNaN(listingPrice)) {
-    document.getElementById('listingPriceDisplay').innerHTML = "Total Listing Price: Invalid Entry For Input."
+    document.getElementById('listingPriceDisplay').innerHTML = "Total Listing Price: Invalid Entry For Input"
   } else {
     document.getElementById('listingPriceDisplay').innerHTML = "Total Listing Price: $" + listingPrice;
     document.getElementById('listingPrice').setAttribute('value', listingPrice);
