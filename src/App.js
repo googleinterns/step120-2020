ef0af13af76c34f3af4b95e6258efcbff846b603
Jortiz23 <jortiz23@verizon.net>
2020-07-20 16:35:20 +0000

    Update app with separation of concerns

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React, { Component } from 'react';
 import PostCommentsDisplay from './PostCommentsDisplay/components/PostCommentsDisplay';
-import firebase from 'firebase/app';
-import 'firebase/database;';
 import './App.css';
 
 class App extends Component {
-    constructor(props){
-        super(props);
-
-        const config ={
-            
-        }
-    }
 
     render(){
         return (
---
+++ src/PostCommentsDisplay/components/PostCommentsDisplay.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Comment from '../../Comment/components/Comment';
+import CommentEditor from '../../CommentEditor/components/CommentEditor';
+
+class PostCommentsDisplay extends Component {
+    constructor(props) {
+        super(props);
+
+        this.addComment = this.addComment.bind(this);
+
+        this.state = {
+            comments: [],
+        }
+    }
+
+    addComment(newCommentBody){
+        const newState = Object.assign({}, this.state);
+        newState.comments.push(newCommentBody);
+        this.setState(newState);
+    }
+
+    render(){
+        return (
+            <div>
+                { 
+                    this.state.comments.map((commentBody, i) => {
+                        return (
+                            <Comment key={i} commentBody={commentBody}/>
+                        )
+                    })
+                }
+                <CommentEditor addComment={this.addComment} />
+            </div>
+        )
+    }
+}
+
+export default PostCommentsDisplay;
