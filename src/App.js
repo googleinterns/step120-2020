dcb1fc9aa0a8e23bc06d174a75253e76e51a7153
Jortiz23 <jortiz23@verizon.net>
2020-07-20 16:29:08 +0000

    Restructure css stylesheets to be component specific

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import PostCommentsDisplay from './PostCommentsDisplay/components/PostCommentsDisplay';
-
+import firebase from 'firebase/app';
+import 'firebase/database;';
 import './App.css';
 
 class App extends Component {
-    
+    constructor(props){
+        super(props);
+
+        const config ={
+            
+        }
+    }
 
     render(){
         return (
