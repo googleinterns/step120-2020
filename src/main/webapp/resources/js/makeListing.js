d49224239bb93cc8797ff6812d5d376f308bf3c0
Shaina Chen <shaina.sierra@gmail.com>
2020-07-20 17:37:26 +0000

    Refactor and add comments:

--- src/main/webapp/resources/js/makeListing.js
+++ src/main/webapp/resources/js/makeListing.js
@@ -1,9 +1,14 @@
+/**
+* Calculates the total listing price.
+* Method is called anytime there is an input change to an input that is
+* involved in this calculation.
+*/
 function calculateListingPrice() {
-  let numSingles = parseInt(document.getElementById("numSingles").value);
-  let singlePrice = parseFloat(document.getElementById("singlePrice").value);
-  let numShared = parseInt(document.getElementById("numShared").value);
-  let sharedPrice = parseFloat(document.getElementById("sharedPrice").value);
-  let listingPrice = numSingles * singlePrice + numShared * sharedPrice;
+  const numSingles = parseInt(document.getElementById("numSingles").value);
+  const singlePrice = parseFloat(document.getElementById("singlePrice").value);
+  const numShared = parseInt(document.getElementById("numShared").value);
+  const sharedPrice = parseFloat(document.getElementById("sharedPrice").value);
+  const listingPrice = numSingles * singlePrice + numShared * sharedPrice;
   if (isNaN(listingPrice)) {
     document.getElementById('listingPriceDisplay').innerHTML = "Total Listing Price: Invalid Entry For Input"
   } else {
