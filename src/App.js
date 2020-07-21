c9f0e9c7c951718086d844cb79676075037fe41c
Jortiz23 <jortiz23@verizon.net>
2020-07-21 17:17:39 +0000

    Change from using react router to reach router to match Mycale's choice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Link } from '@reach/router';
 
 import Home from './Home/components/Home';
 import MakeListingDisplay from './MakeListingDisplay/components/MakeListingDisplay';
-import Navigation from './Navigation';
 
 import './App.css';
 
 class App extends Component {
     render(){
         return (
+            <div>
+                <h1>Roomies</h1>
 
-                <BrowserRouter>
-                    <Navigation />
-                    <Switch>
-                        <Route path="/" component={Home} exact/>
-                        <Route path="/makeListing" component={MakeListingDisplay}/>
-                    </Switch>
-                </BrowserRouter>
+                <nav>
+                    <Link to="/">Home</Link> {" "}
+                    <Link to="/makeListing">Make a Listing</Link>
+                </nav>
 
+                <Router>
+                    <Home path="/" />
+                    <MakeListingDisplay path="/makeListing" />
+                </Router>
+            </div>
         );
     }
 }
