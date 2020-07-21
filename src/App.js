90eb6f22d4792d34e885bfeefacce519c784373f
Jortiz23 <jortiz23@verizon.net>
2020-07-21 20:30:44 +0000

    Alphabetize stylesheets and add class-level documentation comments per PR

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PostCommentsDisplay from './PostCommentsDisplay/components/PostCommentsDisplay';
 import './App.css';
 
+/**Component class to render the whole comments page */
 class App extends Component {
     render(){
         return (
