f8453cfdfe1c951bae367ba446568b45667ee321
Shaina Chen <shaina.sierra@gmail.com>
2020-07-22 23:50:59 +0000

    Change tests to verify servlet behavior

--- src/test/java/com/google/roomies/servlets/ListingsDatabaseTest.java
+++ src/test/java/com/google/roomies/servlets/ListingsDatabaseTest.java
@@ -1,3 +1,4 @@
+ 
 package com.google.roomies;
 
 import static com.google.roomies.ListingConstants.LISTING_COLLECTION_NAME;
@@ -35,6 +36,7 @@ import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 import com.google.roomies.database.Database;
+import com.google.roomies.database.FirebaseDatabase;
 import com.google.roomies.database.DatabaseFactory;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -58,37 +60,34 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 @RunWith(JUnit4.class)
-public class ListingsServletTest {
+public class ListingsDatabaseTest {
   @Mock CollectionReference collectionMock;
   @Mock Firestore dbMock;
   @Mock HttpServletRequest request;
   @Mock HttpServletResponse response;
-  @Mock Database db;
-  ApiFuture<DocumentReference> apiFuture;
-  @Mock DocumentReference queryDocument;
   private Listing listing;
-  private ListingsServlet listingsServlet;
+  private Database db;
   
   @Before
   public void setUp() throws Exception {    
     MockitoAnnotations.initMocks(this);
 
-    listingsServlet = new ListingsServlet();
-    listingsServlet.init();
+    db = DatabaseFactory.getDatabase();
+    db.setDatabaseForTest(dbMock);
+    when(dbMock.collection(LISTING_COLLECTION_NAME)).thenReturn(collectionMock);
 
-    DatabaseFactory.setDatabaseForTest(db);
     setRequestParameters();
   }
 
   @Test
-  public void testPost_postsSingleListing() throws Exception {
+  public void testAddDocumentAsMap_postsSingleListing() throws Exception {
     listing = Listing.fromServletRequest(request);
     Map<String, Object> expectedData = listing.toMap();
-    when(db.addDocumentAsMap(LISTING_COLLECTION_NAME, listing)).thenReturn(apiFuture);    
     
-    listingsServlet.doPost(request, response);
-    
-    verify(db, Mockito.times(1)).addDocumentAsMap(LISTING_COLLECTION_NAME, listing);
+    db.addDocumentAsMap(LISTING_COLLECTION_NAME, response);
+
+    verify(dbMock, Mockito.times(1)).collection(LISTING_COLLECTION_NAME);
+    verify(collectionMock, Mockito.times(1)).add(expectedData);
   }
 
   @Test
@@ -100,7 +99,6 @@ public class ListingsServletTest {
 
     listingsServlet.doPost(request, response);
 
-    verify(db, Mockito.times(0)).addDocumentAsMap(eq(LISTING_COLLECTION_NAME), any(Listing.class));
     verify(response).setStatus(400);
   }
 
@@ -111,7 +109,6 @@ public class ListingsServletTest {
     
     listingsServlet.doPost(request, response);
 
-    verify(db, Mockito.times(0)).addDocumentAsMap(eq(LISTING_COLLECTION_NAME), any(Listing.class));
     verify(response).setStatus(400);
   }
 
@@ -119,10 +116,9 @@ public class ListingsServletTest {
   public void testPost_requestHasInvalidListingPrice_servletResponseIsSetToBadRequest() throws Exception {
     String invalidListingPrice = "price";
     when(request.getParameter(LISTING_PRICE)).thenReturn(invalidListingPrice);
- 
+    
     listingsServlet.doPost(request, response);
 
-    verify(db, Mockito.times(0)).addDocumentAsMap(eq(LISTING_COLLECTION_NAME), any(Listing.class));
     verify(response).setStatus(400);
   }
 
@@ -133,7 +129,6 @@ public class ListingsServletTest {
     
     listingsServlet.doPost(request, response);
 
-    verify(db, Mockito.times(0)).addDocumentAsMap(eq(LISTING_COLLECTION_NAME), any(Listing.class));
     verify(response).setStatus(400);
   }
 
@@ -141,28 +136,17 @@ public class ListingsServletTest {
   public void testPost_requestHasInvalidSinglePrice_servletResponseIsSetToBadRequest() throws Exception {
     String invalidSinglePrice = "-.3";
     when(request.getParameter(SINGLE_ROOM_PRICE)).thenReturn(invalidSinglePrice);
-  
+    
     listingsServlet.doPost(request, response);
 
-    verify(db, Mockito.times(0)).addDocumentAsMap(eq(LISTING_COLLECTION_NAME), any(Listing.class));
     verify(response).setStatus(400);
   }
 
-  /**
-  * Sets mock HTTP request's parameters to corresponding input values.
-  */
-  private void setRequestParameters() {
-    when(request.getParameter(DESCRIPTION)).thenReturn("Test description");
-    when(request.getParameter(END_DATE)).thenReturn("2020-07-10");
-    when(request.getParameter(LEASE_TYPE)).thenReturn("YEAR_LONG");
-    when(request.getParameter(NUM_BATHROOMS)).thenReturn("3");
-    when(request.getParameter(NUM_ROOMS)).thenReturn("2");
-    when(request.getParameter(NUM_SHARED)).thenReturn("1");
-    when(request.getParameter(NUM_SINGLES)).thenReturn("0");
-    when(request.getParameter(SHARED_ROOM_PRICE)).thenReturn("100");
-    when(request.getParameter(SINGLE_ROOM_PRICE)).thenReturn("0");
-    when(request.getParameter(LISTING_PRICE)).thenReturn("100");
-    when(request.getParameter(START_DATE)).thenReturn("2020-07-10");
-    when(request.getParameter(TITLE)).thenReturn("Test title");
+  @Test
+  public void testPost_databaseDoesNotExist_servletResponseIsSetToBadRequest() throws Exception {
+    db.setDatabaseForTest(null);
+    
+    listingsServlet.doPost(request, response);
+
+    verify(response).setStatus(400);
   }
-}
