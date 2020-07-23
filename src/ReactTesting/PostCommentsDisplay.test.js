9e1e3336b2e92712e1a2d10dd4838ca6938a8483
Jortiz23 <jortiz23@verizon.net>
2020-07-23 18:11:38 +0000

    Fix version control error with jest and babel

--- src/ReactTesting/PostCommentsDisplay.test.js
+++ src/ReactTesting/PostCommentsDisplay.test.js
@@ -5,8 +5,22 @@ import CommentEditor from '../PostCommentsDisplay/components/CommentEditor.jsx';
 import Comment from '../PostCommentsDisplay/components/Comment.jsx';
 
 test('PostCommentDisplay renders correctly', () => {
-  const tree = renderer
-    .create(<PostCommentDisplay />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+    const tree = renderer
+        .create(<PostCommentsDisplay />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('CommentEditor renders correctly', () => {
+    const tree = renderer
+        .create(<CommentEditor />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('Comment renders correctly', () => {
+    const tree = renderer
+        .create(<Comment commentBody="Hello There!"/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
 });
