0bccdbf2d5278a4c0ea5e2adfcd7346d0f1af008
Shaina Chen <shaina.sierra@gmail.com>
2020-07-27 16:37:06 +0000

    Create test file for comments database functionality

---
+++ src/test/java/com/google/roomies/servlets/CommentsDatabaseTest.java
@@ -0,0 +1,22 @@
+package com.google.roomies;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+public class CommentsDatabaseTest {
+
+}
