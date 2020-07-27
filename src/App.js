e7e12fa037741015c939df2a25f21b122788d52d
Mycale Radcliffe <mcradcliffe@google.com>
2020-07-27 14:27:29 +0000

    Adds changes to development branch to push

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
-import PostCommentsDisplay from './PostCommentsDisplay/components/PostCommentsDisplay';
+import React from "react";
 import './App.css';
+import SignInPage from "./AuthenticationApp/MainComponents/SignInPage";
+import SignUpPage from "./AuthenticationApp/MainComponents/SignUpPage";
+import { Router } from "@reach/router";
+import UserWrappedHome from "./AuthenticationApp/MainComponents/UserWrappedHome";
 
 /**Component class to render the whole comments page */
-class App extends Component {
-    render(){
-        return (
-            <PostCommentsDisplay />
-        );
-    }
+function App () {
+    return (
+        <Router>
+            <UserWrappedHome path= "/" />
+            <SignInPage path= "signIn" />
+            <SignUpPage path= "signIn/signUp" />
+        </Router>
+    )
 }
 
 export default App;
