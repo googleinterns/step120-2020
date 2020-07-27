95bcbcce6930032abc8c9deb71a9ab6b47aface3
Mycale Radcliffe <mcradcliffe@google.com>
2020-07-27 14:20:43 +0000

    Integrates Authentication with project

---
+++ src/AuthenticationApp/MainComponents/NotLoggedInBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { navigate } from "@reach/router";
+function NotLoggedInBar() {
+  return (
+    <div>
+        <button onClick ={() => {navigate("signIn")}}>Login</button>
+    </div>
+  );
+}
+export default NotLoggedInBar;
---
+++ src/AuthenticationApp/firebase.js
@@ -0,0 +1,40 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/analytics";
+ 
+const firebaseConfig = {
+    apiKey: "AIzaSyAjse2YjCJHUUe5w1i7I9jkjH8cqqRcAIU",
+    authDomain: "step120-2020.firebaseapp.com",
+    databaseURL: "https://step120-2020.firebaseio.com",
+    projectId: "step120-2020",
+    storageBucket: "step120-2020.appspot.com",
+    messagingSenderId: "994372108603",
+    appId: "1:994372108603:web:cb5d95fa2ec4d107b1b71e",
+    measurementId: "G-C0ZHMWXRWH"
+  };
+ 
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+ 
+  export const generateUserDocument = async (user, furtherData) => {
+      if(!user) return;
+      
+      //fill doc with user data
+      
+      return getUserDocument(user.uid);
+  };
+  const getUserDocument = async uid => {
+      if (!uid) return null;
+      try {
+          const userDocument = await firestore.doc(`users/${uid}`).get();
+          return {
+              uid,
+              ...userDocument.data()
+          };
+      } catch (error) {
+          console.error("Error fetching user", error);
+      }
+  };
+  export const authentication = firebase.auth();
+  export const firestore = firebase.firestore();
