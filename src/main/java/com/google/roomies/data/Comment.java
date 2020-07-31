eb84749ef315ad5de4353f99d2ade47b4c0f8187
Shaina Chen <shaina.sierra@gmail.com>
2020-07-31 01:19:37 +0000

    Remove listing id references in class

--- src/main/java/com/google/roomies/data/Comment.java
+++ src/main/java/com/google/roomies/data/Comment.java
@@ -1,7 +1,6 @@
 package com.google.roomies;
  
 import static com.google.roomies.CommentRequestParameterNames.COMMENT;
-import static com.google.roomies.CommentRequestParameterNames.LISTING_ID;
 import static com.google.roomies.CommentRequestParameterNames.TIMESTAMP;
 
 import com.google.api.core.ApiFuture;
@@ -23,8 +22,7 @@ import javax.servlet.http.HttpServletRequest;
 public abstract class Comment {
   abstract Optional<String> documentId();
   abstract Optional<Timestamp> timestamp();
-  abstract String listingId();
-  abstract String comment();
+  public abstract String comment();
  
   public static Builder builder() {
     return new AutoValue_Comment.Builder();
@@ -34,38 +32,9 @@ public abstract class Comment {
   public abstract static class Builder implements Serializable {
     public abstract Builder setTimestamp(Optional<Timestamp> timestamp);
     public abstract Builder setDocumentId(Optional<String> documentId);
-    public abstract Builder setListingId(String listingId);
     public abstract Builder setComment(String comment);
 
-    abstract Comment autoBuild();
-
-    /**
-    * Builds Comment instance.
-    * 
-    * Verifies that listing id for the comment exists in database. If it does not,
-    * Comment instance is not created and exception is thrown.
-    */
-    public Comment build() throws IOException, InterruptedException,
-        ExecutionException {
-      Comment comment = autoBuild();
-      String errorMessage = String.format("Listing with id %s does not exist in " +
-        "database. Comment with message %s cannot be created.", comment.listingId(),
-        comment.comment());
-      Preconditions.checkState(listingWithIdExists(comment.listingId()), errorMessage);
-      return comment;
-    }
-
-    /**
-    * Checks if listing ID exists in database.
-    *
-    * @return true if ID exists, false otherwise
-    */
-    private boolean listingWithIdExists(String listingId) throws IOException,
-        InterruptedException, ExecutionException {
-      ApiFuture<DocumentSnapshot> listing = 
-        DatabaseFactory.getDatabase().getListing(listingId);
-      return listing.get().exists();
-    }
+    public abstract Comment build();
   }
 
   /**
@@ -74,7 +43,6 @@ public abstract class Comment {
   public static Comment fromServletRequest(HttpServletRequest request) throws
       IOException, InterruptedException, ExecutionException {
     return Comment.builder()
-      .setListingId(request.getParameter(LISTING_ID))
       .setComment(request.getParameter(COMMENT))
       .build();
   }
@@ -92,7 +60,6 @@ public abstract class Comment {
   */
   public ImmutableMap<String, Object> toMap() {
     return ImmutableMap.<String, Object>builder()
-      .put(LISTING_ID, listingId())
       .put(COMMENT, comment())
       .put(TIMESTAMP, FieldValue.serverTimestamp())
       .build();
