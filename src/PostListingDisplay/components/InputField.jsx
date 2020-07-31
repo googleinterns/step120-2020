db06e8bd50f8a306caca556e51835734e46fe2f6
Jortiz23 <jortiz23@verizon.net>
2020-07-31 23:23:33 +0000

    Fix spacing error in addition to fit style guides

--- src/PostListingDisplay/components/InputField.jsx
+++ src/PostListingDisplay/components/InputField.jsx
@@ -31,7 +31,7 @@ function setDefault(type){
         case "date":
             const today = new Date();
             let day = today.getDate();
-            let month = today.getMonth()+1;
+            let month = today.getMonth() + 1;
             const year = today.getFullYear();
             if(day < 10) {
                 day = '0' + day;
