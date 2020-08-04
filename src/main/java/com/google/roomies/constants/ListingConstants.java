c980f03b1d49de27331d0e5225958137ca2303a2
Shaina Chen <shaina.sierra@gmail.com>
2020-08-04 23:45:50 +0000

    Add constants for location and distance

--- src/main/java/com/google/roomies/constants/ListingConstants.java
+++ src/main/java/com/google/roomies/constants/ListingConstants.java
@@ -1,7 +1,12 @@
 package com.google.roomies;
 
+import com.google.common.primitives.ImmutableDoubleArray;
+
 public final class ListingConstants {
   private ListingConstants() {}
+  public static final ImmutableDoubleArray BERKELEY_LOCATION = ImmutableDoubleArray.of(37.8719, -122.2585); //[latitude, longitude]
+  public static final double EARTH_RADIUS = 3958.8; //in miles
+  public static final double DEGREE_TO_RADIAN_RATIO = Math.PI / 180;
   public static final String LISTING_COLLECTION_NAME = "listings";
   public static final String CURRENCY_CODE = "USD";
   public static final String DATE_FORMAT = "yyyy-MM-dd";
