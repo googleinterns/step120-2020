11d2312a9ea33eab2a6213d566db9010b107086c
Shaina <shaina.sierra@gmail.com>
2020-08-18 17:16:49 -0700

    Add javadoc comment to class

--- src/main/java/com/google/roomies/constants/ListingConstants.java
+++ src/main/java/com/google/roomies/constants/ListingConstants.java
@@ -1,11 +1,12 @@
 package com.google.roomies;
 
-import com.google.cloud.firestore.GeoPoint;
-
+/** Constants for a listing. */
 public final class ListingConstants {
   private ListingConstants() {}
-  public static final GeoPoint BERKELEY_LOCATION = 
-    new GeoPoint(/* latitude= */ 37.8719, /* longitude= */ -122.2585);
+  public static final Location BERKELEY = Location.builder()
+    .setLatitude(37.8719)
+    .setLongitude(-122.2585)
+    .build();
   public static final double EARTH_RADIUS_IN_MILES = 3958.8;
   public static final double RADIANS_PER_DEGREE = Math.PI / 180;
   public static final String LISTING_COLLECTION_NAME = "listings";
