f3075129e03e809a80b1acb8199c4fdb4a4289be
Shaina <shaina.sierra@gmail.com>
2020-08-30 15:13:00 -0700

    Add distance calculation method to another location

--- src/main/java/com/google/roomies/data/Location.java
+++ src/main/java/com/google/roomies/data/Location.java
@@ -1,5 +1,12 @@
 package com.google.roomies;
 
+import static com.google.roomies.LocationConstants.RADIANS_PER_DEGREE;
+import static com.google.roomies.LocationConstants.EARTH_RADIUS_IN_MILES;
+import static java.lang.Math.asin;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
+
 import com.google.auto.value.AutoValue;
 import com.google.cloud.firestore.GeoPoint;
 import com.google.common.base.Preconditions;
@@ -42,5 +49,28 @@ public abstract class Location {
   */
   public GeoPoint toGeoPoint() {
     return new GeoPoint(latitude(), longitude());
+  }
+  
+  /**
+  * Calculates the straight-line distance in miles between two locations.
+  *
+  * Uses the Haversine distance (angular distance between two points
+  * on the surface of a sphere).
+  *
+  * Note: Driving/walking distance was not implemented at this point in time because using
+  * the Google Places API requires displaying the results on a Google Map, which is 
+  * out of scope for our MVP.
+  */
+  public double distanceTo(Location location) {
+    double firstLocationLatitudeInRadians = this.latitude() * RADIANS_PER_DEGREE; 
+    double secondLocationLatitudeInRadians = location.latitude() * RADIANS_PER_DEGREE; 
+    double latitudeDifference = secondLocationLatitudeInRadians - firstLocationLatitudeInRadians;
+    double longitudeDifference = (location.longitude() - this.longitude()) 
+      * RADIANS_PER_DEGREE;
+
+    return 2 * EARTH_RADIUS_IN_MILES * asin(sqrt(sin(latitudeDifference/2.0) *
+      sin(latitudeDifference/2.0) + cos(firstLocationLatitudeInRadians) * 
+      cos(secondLocationLatitudeInRadians) * sin(longitudeDifference/2.0) * 
+      sin(longitudeDifference/2.0)));
   }
 }
